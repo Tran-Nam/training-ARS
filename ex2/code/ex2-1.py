4bed774bc5d06479bafdccef3be47878e4e33abd
Tran-Nam <phuongnam9613@gmail.com>
2018-08-02 11:04:58 +0700

    ex2

--- ex2/code/ex2-1.py
+++ ex2/code/ex2-1.py
@@ -15,8 +15,7 @@ with open(path) as f:
 # print(Y)
 X = np.array(X)
 Y = np.array(Y)
-# print(X.shape)
-# print(Y.shape)
+
 
 
 # visualize data
@@ -28,10 +27,76 @@ plt.plot(X[neg][:, 0], X[neg][:, 1], 'bo', label='Not admitted')
 plt.xlabel('Exam 1 score')
 plt.ylabel('Exam 2 score')
 plt.legend()
-plt.show()
+# plt.show()
 # print(X_1)
 
+m = X.shape[0]
+ones = np.array([[1] for _ in range(m)])
+X = np.column_stack((ones, X))
+print(X.shape)
+print(Y.shape)
+
+
 # sigmoid function
-def sigm(z):
-    return 1./(1 + np.exp(-z))
+# def sigm(z):
+#     return 1/(1 + np.exp(-z))
+
+# def scoreFunc(w):
+#     return sigm(X.dot(w))
+
+# def computeCost(w):
+#     return np.sum(-Y.T.dot(np.log(scoreFunc(w))) - (1 - Y).T.dot(np.log(1 - scoreFunc(w))))
+
+# def grad(w):
+#     return X.T.dot(scoreFunc(w) - Y)/m 
+
+# def GD(w_init, grad, lr):
+#     cost = []
+    
+#     w_current = w_init
+#     for it in range(ite):
+#         w_new = w_current - lr * grad(w_current)
+#         w_current = w_new
+#         # cost.append(computeCost(w_new))
+#         # print(w_new, cost[-1])
+#         # print(w_current)
+#     return w_new#, cost
+
+# lr = .1
+# w_init = np.array([[0], [0], [0]])
+# ite = 1000000
+
+# w_op = GD(w_init, grad, lr)
+# print(w_op)
+
+# x1 = np.arange(30, 100, .5)
+# x2 = -(w_op[0][0] + w_op[1][0] * x1)/w_op[2][0]
+# plt.plot(x1, x2, 'r-')
+# plt.show()
+
+
+def predict(w):
+    y_pred = X.dot(w)
+    return 1. / (1 + np.exp(-y_pred))
+
+def grad(w):
+    return 1. / m * (X.T.dot(predict(w) - Y))
+
+def GD(w_init, lr, ite):
+    w = w_init
+    for it in range(ite):
+        w = w - lr*grad(w)
+    return w
+
+w_init = np.array([[0], [0], [0]])
+lr = .3
+ite = 1000000
+
+w_op = GD(w_init, lr, ite)
+print(w_op)
+    
+x1 = np.arange(30, 100, .5)
+x2 = -(w_op[0][0] + w_op[1][0] * x1)/w_op[2][0]
+plt.plot(x1, x2, 'r-')
+plt.show()
 
--- ex2/code/ex2-2.py
+++ ex2/code/ex2-2.py
@@ -14,12 +14,58 @@ with open(path) as f:
 X = np.array(X)
 Y = np.array(Y)
 
+
+
 pos = np.where(Y == 1)[0]
 neg = np.where(Y == 0)[0]
 
-plt.plot(X[pos][:, 0], X[pos][:, 1], 'k+', label='y = 1')
+plt.plot(X[pos][:, 0], X[pos][:, 1], 'r^', label='y = 1')
 plt.plot(X[neg][:, 0], X[neg][:, 1], 'bo', label='y = 0')
 plt.legend()
 plt.xlabel('Microchip test 1')
 plt.ylabel('Microchip test 2')
-plt.show()
+# plt.show()
+
+
+# print(X.shape)
+# print(Y.shape)
+m = X.shape[0]
+
+x1 = X[:,0]
+x2 = X[:,1]
+deg = 6
+X_train = np.ones((m, 1))
+for i in range(1, deg+1):
+    for j in range(i+1):
+        X_train = np.hstack((X_train, ((x1**(i-j))*(x2**j)).reshape(m,1)))
+# print(X_train.shape)
+m = X_train.shape[0]
+n = X_train.shape[1]
+# print(m)
+# print(n)
+
+# print(X_train[:,0])
+def predict(w):
+    z = X_train.dot(w)
+    return 1 / (1 + np.exp(-z))
+
+def GD(w_init, lr, ite, alpha):
+    w = w_init
+    for it in range(ite):
+        w[0] = w[0] - lr*X_train[:,0].dot(predict(w)- Y)
+        for i in range(1, n):
+            w[i] = w[i] - lr*(X_train[:, i].dot(predict(w) - Y)/m + alpha*w[i]/m)
+        if it%100 == 0:
+            print(np.abs(predict(w) - Y).mean())
+    return w
+
+
+lr = .1
+alpha = .1
+ite = 1000
+w_init = np.zeros((n,1)).reshape((n,1))
+w_op = GD(w_init, lr, ite, alpha)
+print(np.abs(predict(w_op) - Y).mean())
+    
+# print(X_train)
+# print(X_train.shape)
